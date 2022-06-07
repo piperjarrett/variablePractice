42d08287c7a0b2fd386b51e83e7dce0a9896ae85
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-06-07 15:24:25 -0600

    Try again with all variables

--- variable.js
+++ variable.js
@@ -1,20 +1,18 @@
-# String Variable
 
-1. var name = 'piper'
-2. var title = 'hello'
-2. var weather = 'sunny'
+var name = 'piper'
+var title = 'hello'
+var weather = 'sunny'
 
-# Number Variable
-1. var age = '21'
-1. var temp = '75'
-1. var time = '1500'
+var age = '21'
+var temp = '75'
+var time = '1500'
 
-# Booleans Variable
-1. var online = 'true'
-1. var wifi = 'false'
-1. var payment = 'true'
 
-# New Assiged Variables
-1. var age = '34'
-1. var temp = '82'
-1. var online = 'false'
+var online = 'true'
+var wifi = 'false'
+var payment = 'true'
+
+
+var age = '34'
+var temp = '82'
+var online = 'false'
